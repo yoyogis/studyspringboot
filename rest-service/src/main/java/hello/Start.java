233016ec544cc9f22c9ec447c0d5c4a109b4a928
yuanyu <yuanyu002@gmail.com>
2019-03-10 15:34:23 +0800

    添加一个rest-service项目

---
+++ rest-service/src/main/java/hello/Start.java
@@ -0,0 +1,11 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class  Start{
+    public static  void main(String[] args){
+        SpringApplication.run(Start.class, args);
+    }
+}
